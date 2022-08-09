281ef0645d13abb31eaee617fae8d4ca270139ae
cbolla <futuro.noob@gmail.com>
2022-08-09 19:54:46 -0300

    Fix: arquivo pra organizar variaveis

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const puppeteer = require('puppeteer-core')
 const UserAgent = require('user-agents')
+const profiles = require("./profiles")
+
+const marc1 = profiles.profiles
 
 const Comment = async ()=>{
     const navegator = await puppeteer.launch({
         headless: false,
-        executablePath: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+        executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
     })
     const page = await navegator.newPage()
     const userAgent = new UserAgent();
@@ -12,7 +15,6 @@ const Comment = async ()=>{
     await page.goto("https://www.instagram.com/")
     await page.waitForTimeout(35000)
     await page.goto("https://www.instagram.com/p/Cg2XTNuBNjG/")
-    let marc1 = ["@willian_reis18 @william_fabianojd","@_bubble_styles @noemioliveira295","@noemioliveira295 @william_fabianojd","@willian_reis18 @_bubble_styles","@_bubble_styles @noemioliveira295","@william_fabianojd @_bubble_styles","@nathalya.ayal @lilianmarinho27","@susihahns @sousajayyene","@laneoliver29 @soucleison_","@laristefannysz @isabelinha168","@isabelinha168 @estevaoeloiza","@lisangelafbrandao @eduardapedrosa25","@helenasantana01 @letyoficial31","@xxbruna30 @queel_lovatic","@jacira6506 @maniafemininaa_makeup","@bgmslmg @silva_sefa","@ribeirojack2 @flayaparecida","@123railane @leslye.medeiros","@nana85silva @loira_paulinha","@danii_silva2 @maria200888501","@samarasyllva @heidyalvesda","@akillanathiely.limaa @claudiadanielada","@kayla_freire @danielelagassigonzaga","@helia.silva23 @eleide.souza","@danielacandidomakeup @marciateixxeira","@lucianaewan @niicole.ssantos","@amandacris747    @edinalva_1010","@alanna_mano @vivi_sl28"]
     let temp = []
     for(let x = 30000 ; x < 60000; x++){
         temp.push(x)
---
+++ src/profiles.js
@@ -0,0 +1,5 @@
+let profiles = ["@willian_reis18 @william_fabianojd","@_bubble_styles @noemioliveira295","@noemioliveira295 @william_fabianojd","@willian_reis18 @_bubble_styles","@_bubble_styles @noemioliveira295","@william_fabianojd @_bubble_styles","@nathalya.ayal @lilianmarinho27","@susihahns @sousajayyene","@laneoliver29 @soucleison_","@laristefannysz @isabelinha168","@isabelinha168 @estevaoeloiza","@lisangelafbrandao @eduardapedrosa25","@helenasantana01 @letyoficial31","@xxbruna30 @queel_lovatic","@jacira6506 @maniafemininaa_makeup","@bgmslmg @silva_sefa","@ribeirojack2 @flayaparecida","@123railane @leslye.medeiros","@nana85silva @loira_paulinha","@danii_silva2 @maria200888501","@samarasyllva @heidyalvesda","@akillanathiely.limaa @claudiadanielada","@kayla_freire @danielelagassigonzaga","@helia.silva23 @eleide.souza","@danielacandidomakeup @marciateixxeira","@lucianaewan @niicole.ssantos","@amandacris747    @edinalva_1010","@alanna_mano @vivi_sl28"]
+
+module.exports={
+    profiles
+}
